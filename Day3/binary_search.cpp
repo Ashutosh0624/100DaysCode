8ec31edf34c599d09aec2d2c027576dce562b108
Ashutosh0624 <ashutosh.phd0602@gmail.com>
2025-03-18 05:56:24 +0000

    Updated binary search

--- Day3/binary_search.cpp
+++ Day3/binary_search.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 int binary_search(std::vector<int>&v1, int key){
+    // Binary search only works on sorted arrays.
+    sort(v1.begin(), v1.end());
+
     int low = 0;
     int high = v1.size() - 1;
     while(low <= high){
