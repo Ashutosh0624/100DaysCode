fb46b7081140275f9644df9822904431ee97e01e
Ashutosh0624 <ashutosh.phd0602@gmail.com>
2025-03-18 05:52:41 +0000

    binary search

---
+++ Day3/binary_search.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binary_search(std::vector<int>&v1, int key){
+    int low = 0;
+    int high = v1.size() - 1;
+    while(low <= high){
+        int mid = (low + high)/2;
+        if(v1[mid] == key) return mid;
+        if (v1[mid] < key) {low = mid + 1;}
+        if (v1[mid] > key) {high = mid - 1;}
+
+    }
+    return -1;
+}
+int main(){
+    std::vector<int>v1{12, 45, 88, -9, 99, -1};
+    int key;
+    cin>>key;
+    
+    int index = binary_search(v1, key);
+    if(index != -1){
+        cout<<"element found !! at index : "<<index<<"th "<<endl;
+    }
+    else{
+        cout<<"Element not found!!"<<endl;
+    }
+    return  0;
+}
