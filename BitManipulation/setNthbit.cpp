04b4ee6eae7a87e8e284c2b87c7419b6489467b2
Ashutosh0624 <ashutosh.phd0602@gmail.com>
2025-03-19 11:42:17 +0000

    update bit manipulation

--- BitManipulation/setNthbit.cpp
+++ BitManipulation/setNthbit.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 int setNthbit(int n, int pos){
-    n = n & (1 << pos);
+    n = n | (1 << pos);
     return n;
+}
+
+int clearNthBit(int n, int pos){
+    return (n & (~(1 << pos)));
 }
 
 int main(){
     int n = 16;
-    cout<<setNthbit(n, 1);
-    cout<<endl;
+    cout<<setNthbit(n, 3)<<endl;
+    cout<<clearNthBit(n, 3)<<endl;
     return 0;
 }
